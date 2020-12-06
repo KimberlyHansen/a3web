5e5a7cfed203ad1576f3fb988ca22088db525811
aaronjutzi <ajutzi@flemingcollege.ca>
2020-12-05 21:10:06 -0500

    js

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-function time(x){
-    var d = new Date();
+function perth(x){
+    var d = new Date()
     var utc = d.toUTCString(x)
     document.write(utc);
-    }
+}
+
+function eucia(){
+    var d = new Date()
+    var utc = d.toUTCString(8.75)
+    document.write(utc); 
+}
+
+function darwin(){
+    var d = new Date()
+    var utc = d.toUTCString(9.5)
+    document.write(utc);
+}
+
+function brisbane(){
+    var d = new Date()
+    var utc = d.toUTCString(10)
+    document.write(utc);
+}
+
+function adelaide(){
+    var d = new Date()
+    var utc = d.toUTCString(10.5)
+    document.write(utc);
+}
+
+function sydney(){
+    var d = new Date()
+    var utc = d.toUTCString(11)
+    document.write(utc);
+}
