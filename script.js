13b277c9915c22df9217a25e09494ae680c3e346
aaronjutzi <ajutzi@flemingcollege.ca>
2020-12-06 16:19:51 -0500

    js

--- script.js
+++ script.js
@@ -27,8 +27,14 @@ var sticky = navbar.offsetTop;
 
 function navbarSticky() {
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+        navbar.className += ("sticky")
     } else {
         navbar.classList.remove("sticky");
     }
+//function navbarSticky() {
+//    if (window.pageYOffset >= sticky) {
+//        navbar.classList.add("sticky")
+//    } else {
+ //       navbar.classList.remove("sticky");
+ //   }
 }
