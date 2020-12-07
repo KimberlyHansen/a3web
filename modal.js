d584eef9bff5e685fcbf0f31717bb1a08d1d0d90
KimberlyHansen <kimberly.hansen@flemingcollege.ca>
2020-12-07 12:36:05 -0500

    javascript test for modal

--- modal.js
+++ modal.js
@@ -1,7 +1,7 @@
 
 // Javascript for Modal built by Kimberly
-var modal = document.getElementById("insidemodal");
-var btn = document.getElementById("Inside");
+var modal = document.getElementById("insidemodal","outsidemodal");
+var btn = document.getElementById("Inside","Outside");
 var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
