3a7b7dc5e760ac08bc5bd89ca2ab637798aad338
KimberlyHansen <kimberly.hansen@flemingcollege.ca>
2020-12-07 09:49:34 -0500

    Modal image test

--- script.js
+++ script.js
@@ -32,3 +32,19 @@ function navbarSticky() {
         navbar.classList.remove("sticky");
     }
 }
+
+// Javascript for Modal built by Kimberly
+var modal = document.getElementById("insidemodal");
+var btn = document.getElementById("Inside");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
