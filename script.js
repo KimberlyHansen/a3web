d12119bb104047ed295ed3b90718e759d1f4d573
KimberlyHansen <kimberly.hansen@flemingcollege.ca>
2020-12-07 12:59:11 -0500

    sticky navbar added back to time page javascript

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ document.getElementById("aud").innerHTML = aud;
 document.getElementById("aub").innerHTML = aub;
 document.getElementById("aua").innerHTML = aua;
 document.getElementById("aus").innerHTML = aus;
+
+window.onscroll = function() {navbarSticky()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function navbarSticky() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
