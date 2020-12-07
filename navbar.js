6a0775cff6c7862cf820e9aafe70e82497e3b821
KimberlyHansen <kimberly.hansen@flemingcollege.ca>
2020-12-07 11:49:59 -0500

    new navbar javascript file

---
+++ navbar.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {navbarSticky()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function navbarSticky() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
